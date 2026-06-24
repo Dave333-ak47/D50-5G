373226dd6481fe131089e73fdf4b3c1bc4b16479
Dave333-ak47 <davidecavecchia2@gmail.com>
2026-06-24 20:18:04 +0200

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
 
-# 1. Imposta l'IP di default del router (Opzionale: scommenta se vuoi forzarlo a 172.30.55.1)
-# sed -i 's/192.168.1.1/172.30.55.1/g' package/base-files/files/bin/config_generate
+# ============================
+# 1. Forza il target D50
+# ============================
+sed -i 's/CONFIG_TARGET.*/CONFIG_TARGET_ipq50xx=y/' .config
+sed -i 's/CONFIG_TARGET_ipq50xx.*/CONFIG_TARGET_ipq50xx=y/' .config
+sed -i 's/CONFIG_TARGET_ipq50xx_generic.*/CONFIG_TARGET_ipq50xx_generic=y/' .config
 
-# 2. Rimuovi eventuali pacchetti duplicati che potrebbero andare in conflitto
+grep -q "CONFIG_TARGET_ipq50xx_generic_DEVICE_xunison_exigo_d50=y" .config || \
+echo "CONFIG_TARGET_ipq50xx_generic_DEVICE_xunison_exigo_d50=y" >> .config
+
+# ============================
+# 2. Copia i file del target
+# ============================
+mkdir -p target/linux/ipq50xx
+mkdir -p target/linux/ipq50xx/image
+mkdir -p target/linux/ipq50xx/files/arch/arm64/boot/dts/qcom
+
+cp $GITHUB_WORKSPACE/d50/Makefile target/linux/ipq50xx/Makefile
+cp $GITHUB_WORKSPACE/d50/generic.mk target/linux/ipq50xx/generic.mk
+cp $GITHUB_WORKSPACE/d50/image.mk target/linux/ipq50xx/image/Makefile
+
+# ============================
+# 3. Copia il DTS del D50
+# ============================
+cp $GITHUB_WORKSPACE/d50/ipq5018-exigo-hub-d50-5g.dts \
+   target/linux/ipq50xx/files/arch/arm64/boot/dts/qcom/
+
+# ============================
+# 4. Feed modem (come già fai)
+# ============================
 rm -rf package/luci-app-exigod50
-
-# 3. Clona il feed personalizzato per la gestione del modem 5G e del band locking
 git clone https://github.com/Dave333-ak47/d50-modem-feed package/luci-app-exigod50
 
-# 4. Crea la struttura delle cartelle per i firmware statici di OpenWrt
+# ============================
+# 5. Copia firmware WiFi
+# ============================
 mkdir -p package/base-files/files/lib/firmware/
 
-# 5. Iniezione automatica dei file di calibrazione Wi-Fi Bdwlan dell'Exigo D50
-# Cerca i file sia nella root che nella cartella 'wi-fi/' e li copia nella partizione finale
 if [ -d "wi-fi" ]; then
-    echo "Rilevata cartella wi-fi, copia dei file Bdwlan in corso..."
     cp -r wi-fi/Bdwlan* package/base-files/files/lib/firmware/
 else
-    echo "Copia dei file Bdwlan dalla directory principale in corso..."
     cp -r Bdwlan* package/base-files/files/lib/firmware/ 2>/dev/null
 fi
 
-echo "Script diy-part2.sh eseguito con successo!"
+echo "D50: diy-part2.sh completato"
+
